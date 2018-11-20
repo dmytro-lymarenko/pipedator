c4232716b58f548d593c5ffcc5165d9dcef98b29
dmytro.lymarenko <dmytro.lymarenko@gmail.com>
2018-11-20 22:20:44 +0200

    Add nullabel() validator

---
+++ src/nullable/index.ts
@@ -0,0 +1,7 @@
+import { Validator } from '../core';
+import { either } from '../either';
+import { equalTo } from '../equalTo';
+
+export function nullable(validator: Validator, message?: string) {
+	return either([equalTo(null), validator], message);
+}
